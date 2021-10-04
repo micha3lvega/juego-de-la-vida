ab7ed2f0050acfe249732532c7fc0f609a736cbc
Michael Vega <ec3572h@comcel.com.co>
2021-10-03 21:15:29 -0500

    Crear variables globales
    Crear variables globales que se van a usar en el proyecto.

--- js/game.js
+++ js/game.js
@@ -1,1 +1,19 @@
+// Variables globales
+var ctx;
+var canvas;
+var fps = 3;
+
+// Pixeles
+var canvasX = 500; //pixels ancho
+var canvasY = 500; //pixels alto
+var tileX, tileY;
+
+//Variables relacionadas con el tablero de juego
+var tablero;
+var filas = 10;
+var columnas = 10;
+
+var negro = '#000000';
+var blanco = '#FFFFFF';
+
 console.log(':)');
